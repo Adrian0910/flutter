aa75b8d0f1dcce248d7588ad7972fafba9adeb38
Adrian0910 <uchihasantander@gmail.com>
2020-12-15 20:18:12 -0600

    Nuevas opciones, animated container , inputs y avatars

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 //import 'package:componentes/src/pages/home_temp.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/routes/routes.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +12,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        //AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English, no country code
+        const Locale('es', 'ES'), // Arabic, no country code
+        const Locale.fromSubtags(
+            languageCode: 'zh'), // Chinese *See Advanced Locales below*
+        // ... other locales the app supports
+      ],
       //home: HomePage()
       initialRoute: '/',
       routes: getApplicationroutes(),
--- componentes/lib/src/pages/avatar_page.dart
+++ componentes/lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://static2.abc.es/media/play/2019/08/05/spider-man-kB0E--1200x630@abc.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.green[600],
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://i.insider.com/5af099efbd96711f008b460a?width=1300&format=jpeg&auto=webp'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
