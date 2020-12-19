81d09d72dd89bc35a889821ae681df167d807cf2
Adrian0910 <uchihasantander@gmail.com>
2020-12-18 19:42:18 -0600

    Slider Spiderman

--- componentes/lib/src/routes/routes.dart
+++ componentes/lib/src/routes/routes.dart
@@ -6,6 +6,7 @@ import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_pages.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationroutes() {
   return <String, WidgetBuilder>{
@@ -15,5 +16,6 @@ Map<String, WidgetBuilder> getApplicationroutes() {
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
+    'slider': (BuildContext context) => SliderPage(),
   };
 }
